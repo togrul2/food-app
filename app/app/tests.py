7b8c698b0c146e48401d32802c539579f8f54446
togrul2 <adsads00123@gmail.com>
2022-07-28 23:45:34 +0400

    tests added 2.0

--- app/app/tests.py
+++ app/app/tests.py
@@ -10,5 +10,5 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         """Test adding numbers together."""
         res = calc.add(5, 6)
-        
+
         self.assertEqual(res, 11)
