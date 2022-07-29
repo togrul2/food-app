e5cf19f3e373b306308a8a8539c6245406e2f753
togrul2 <adsads00123@gmail.com>
2022-07-30 01:47:11 +0400

    custom user added

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,35 @@
-from django.db import models  # noqa
+"""
+Database models.
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+from django.utils.translation import gettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+    """User manager for user model."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model."""
+    email = models.EmailField(_('email'), max_length=255, unique=True)
+    name = models.CharField(_('name'), max_length=255)
+    is_active = models.BooleanField(_('is active'), default=True)
+    is_staff = models.BooleanField(_('is staff'), default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
